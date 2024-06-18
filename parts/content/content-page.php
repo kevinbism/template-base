a707e7b7de8b4e8a978401bd59ce2dea4743b2ea
Kevin Ramirez <kevin.ramirez@blastness.com>
2024-06-18 11:36:46 +0200

    refactor: nomi file

--- parts/content/content-page.php
+++ parts/content/content-page.php
@@ -1,30 +1,30 @@
 <section class="page p-rel <?= $class ?>">
   <div class="wrapper box-sized">
     <div class="breadcrumb">
-      <?= $this->getBreadCrumb("breadcrumb-list flex flex-wrap flex-ai-c", "breadcrumb-item") ?>
+      <?= $this->getBreadCrumb("breadcrumb-list", "breadcrumb-item") ?>
     </div>
     <?php
     switch ($type) {
       case 'center':
     ?>
-    <div class="page-content text-center">
-      <h2 class="subtitle"><?= $this->getSottotitolo() ?></h2>
-      <h1 class="title"><?= $this->getTitolo() ?></h1>
-      <div class="text"><?= $this->getTesto() ?></div>
-      <?php $this->cube_parts('parts.content.link-page'); ?>
-      <?php $this->cube_parts('parts.landing.page-landing'); ?>
-    </div>
-    <?php
-    break;
-    default:
-    ?>
-    <div class="page-content">
-      <h2 class="subtitle"><?= $this->getSottotitolo() ?></h2>
-      <h1 class="title"><?= $this->getTitolo() ?></h1>
-      <div class="text"><?= $this->getTesto() ?></div>
-      <?php $this->cube_parts('parts.content.link-page'); ?>
-      <?php $this->cube_parts('parts.landing.page-landing'); ?>
-    </div>
+        <div class="page-content text-center">
+          <h2 class="subtitle"><?= $this->getSottotitolo() ?></h2>
+          <h1 class="title"><?= $this->getTitolo() ?></h1>
+          <div class="text"><?= $this->getTesto() ?></div>
+          <?php $this->cube_parts('parts.content.link-page'); ?>
+          <?php $this->cube_parts('parts.landing.page-landing'); ?>
+        </div>
+      <?php
+        break;
+      default:
+      ?>
+        <div class="page-content">
+          <h2 class="subtitle"><?= $this->getSottotitolo() ?></h2>
+          <h1 class="title"><?= $this->getTitolo() ?></h1>
+          <div class="text"><?= $this->getTesto() ?></div>
+          <?php $this->cube_parts('parts.content.link-page'); ?>
+          <?php $this->cube_parts('parts.landing.page-landing'); ?>
+        </div>
     <?php } ?>
   </div>
 </section>
